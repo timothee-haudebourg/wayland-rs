009856f226a583e5d0242d43ca2643327ee3f64b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-16 13:20:22 +0000

    Deploy Smithay/wayland-rs to github.com/Smithay/wayland-rs.git:gh-pages

---
+++ implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["wayland_client"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"wayland_client/sinks/struct.BlockingMsgIter.html\" title=\"struct wayland_client::sinks::BlockingMsgIter\">BlockingMsgIter</a>&lt;T&gt;",synthetic:false,types:["wayland_client::sinks::BlockingMsgIter"]},];
+implementors["wayland_commons"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"wayland_commons/sinks/struct.MsgIter.html\" title=\"struct wayland_commons::sinks::MsgIter\">MsgIter</a>&lt;T&gt;",synthetic:false,types:["wayland_commons::sinks::MsgIter"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,8 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["wayland_client"] = {"name":"","dirs":[{"name":"native_lib","dirs":[],"files":["display.rs","event_queue.rs","mod.rs","proxy.rs"]}],"files":["cursor.rs","display.rs","egl.rs","event_queue.rs","globals.rs","lib.rs","proxy.rs","sinks.rs"]};
+sourcesIndex["wayland_commons"] = {"name":"","dirs":[],"files":["lib.rs","map.rs","sinks.rs","socket.rs","utils.rs","wire.rs"]};
+sourcesIndex["wayland_protocols"] = {"name":"","dirs":[],"files":["lib.rs","misc.rs","protocol_macro.rs","stable.rs","unstable.rs","wlr.rs"]};
+sourcesIndex["wayland_scanner"] = {"name":"","dirs":[],"files":["c_code_gen.rs","c_interface_gen.rs","common_gen.rs","lib.rs","parse.rs","protocol.rs","side.rs","util.rs"]};
+sourcesIndex["wayland_server"] = {"name":"","dirs":[{"name":"native_lib","dirs":[],"files":["client.rs","display.rs","globals.rs","mod.rs","resource.rs"]}],"files":["client.rs","display.rs","globals.rs","lib.rs","resource.rs"]};
+sourcesIndex["wayland_sys"] = {"name":"","dirs":[],"files":["client.rs","common.rs","cursor.rs","egl.rs","lib.rs","server.rs"]};
+createSourceSidebar();
